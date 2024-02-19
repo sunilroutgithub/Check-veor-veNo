a2868d62a033cfa25253486294d4c59d2fea5d1e
sunil_github <117855897+sunilroutgithub@users.noreply.github.com>
2024-02-19 12:58:06 +0530

    Add files via upload

---
+++ CheckNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin >>number;
+    if(number >=0){
+        cout<< number << " is a positive number."<< endl;
+    }else {
+        cout<< number << " is a negative number." <<endl;
+    }
+    return 0;
+}
